d2fe5728a9f0c13af82c67127ff5dd69b322024b
Yan Thomas <yanthomas.b@gmail.com>
2020-11-17 11:05:39 -0300

    set up

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-window.onscroll = function() {fixHeader()}
+window.onscroll = function() {
+	fixHeader();
+};
 
 function fixHeader() {
-    let header = document.getElementById("header")
-    let sticky = header.offsetTop
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky")
-    header.classList.add("has-background-black")
-  } else {
-    header.classList.remove("sticky")
-  }
+  
+	const header = document.getElementById('header');
+	const sticky = header.offsetTop;
+
+	if (window.pageYOffset > sticky) {
+    header.classList.add('sticky');
+    header.classList.add('has-background-purple');
+    
+	} else {
+    header.classList.remove('sticky');
+    header.classList.remove('has-background-purple');
+	}
 }
